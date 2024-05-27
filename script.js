7408b8beb7fcb382095141d5b130ec9719670e63
Sakshi Choudhary <103510144+sakshi-1140@users.noreply.github.com>
2024-05-27 23:48:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //your JS code here. If required.
 let input=document.getElementById("fname")
 input.addEventListener("blur",toUpperCase)
-function toUpperCase{
+function toUpperCase(){
   let str=input.value
   input.value=str.toUpperCase();
 }
