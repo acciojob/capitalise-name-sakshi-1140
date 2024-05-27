e500dac77ccde80b65ac05e6bae3ccdef298e384
Sakshi Choudhary <103510144+sakshi-1140@users.noreply.github.com>
2024-05-27 23:34:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+let input=document.getElementById("fname")
+input.addEventListener("change",toUpperCase)
+function toUpperCase{
+  let value=input.value
+  let input.value=value.toUpperCase();
+}
